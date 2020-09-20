1f810bdd541d4ca51feb1fac16bd06701b898447
malan88 <m@stdwtr.io>
2020-09-20 09:53:20 -0400

    Rename app.elasticsearch to app.es, add insert route

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+    app.es = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
         if app.config['ELASTICSEARCH_URL'] else None
 
     from app.main import bp as main_bp
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,23 @@
-from flask import current_app as ca
+from datetime import datetime
+
+from flask import current_app as app
 
 from app.main import bp
 
 
 @bp.route('/')
 def index():
-    results = ca.elasticsearch.get(index='contents', id='yo')
+    results = app.es.get(index='contents', id='yo')
     return results
+
+@bp.route('/insert')
+def insert():
+    body = {
+        'title': "yo",
+        'content': "Yo, dawg, I heard you like databases, so we put a database"
+                   "in your database.",
+        'timestamp': datetime.now()
+    }
+
+    result = app.es.index(index='contents', id=body['title'], body=body)
+    return result
