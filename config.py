ddf0df14f0de18501ac10583c39aab6c69530114
malan88 <m@stdwtr.io>
2020-09-20 09:53:41 -0400

    Add POSTGRES config and SQLAlchemy import

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
 import os
 
 class Config(object):
+    POSTGRES = {
+        'usr': 'postgres',
+        'pw': 'postgres',
+        'db': 'postgres',
+    }
+
     SECRET_KEY = os.environ.get('SECRET_KEY') \
         or 'you-will-never-guess'
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL') \
         or 'http://es01:9200'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        f'postgresql://{POSTGRES["usr"]}:' \
+        f'{POSTGRES["pw"]}@postgres/${POSTGRES["db"]}'
