c3251860b184ef50bd5bda1feef8d1a8b9744b4a
malan88 <m@stdwtr.io>
2020-10-16 15:17:11 -0400

    Remove insert route (obsolete)

--- app/main/routes.py
+++ app/main/routes.py
@@ -7,17 +7,5 @@ from app.main import bp
 
 @bp.route('/')
 def index():
-    results = app.es.get(index='contents', id='yo')
+    results = app.es.get(index='text', id='1-0')
     return results
-
-@bp.route('/insert')
-def insert():
-    body = {
-        'title': "yo",
-        'content': "Yo, dawg, I heard you like databases, so we put a database"
-                   "in your database.",
-        'timestamp': datetime.now()
-    }
-
-    result = app.es.index(index='contents', id=body['title'], body=body)
-    return result
