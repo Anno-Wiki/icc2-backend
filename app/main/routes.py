bc9e91128131bf16bf3d61c32c2ce34b170aa9a0
malan88 <m@stdwtr.io>
2020-10-20 13:13:15 -0400

    Add toc route/function

--- app/main/routes.py
+++ app/main/routes.py
@@ -9,3 +9,26 @@ from app.main import bp
 def index(toc_id):
     results = app.es.get(index='toc', id=toc_id)
     return results
+
+@bp.route('/toc/<bookid>')
+def toc(bookid):
+    results = app.es.search(
+        index='toc',
+        body={
+            'query': {
+                'bool': {
+                    'must': [
+                        {'match': {'doc.bookid': bookid}},
+                        {'match': {'doc.display': True}}
+                    ]
+                }
+            },
+            'size': 10000
+        }
+    )
+    # get rid of the es cruft
+    results = results['hits']['hits']
+    tocs = [t['_source']['doc'] for t in results]
+    # sort by place in book
+    tocs.sort(key=lambda i: i['open'])
+    return {'results': tocs}
